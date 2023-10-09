53dde60ed120d775591dfb8080fd88ea2f36ac8e
DESKTOP-NR6TVAI\LAPTOP <amiraelsaftytraining@gmail.com>
2023-10-09 19:23:52 +0300

    initial project. added splash screen.

---
+++ lib/home/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routeName = 'home';
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+        centerTitle: true,
+        title: Text('News App',
+        style: Theme.of(context).textTheme.titleLarge,),
+    ),
+    );
+  }
+}
